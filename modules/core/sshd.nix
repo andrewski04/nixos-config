d57aca669dfe0d3e240c0a94507708814747f53b
Andrew <andrew@housermail.com>
2026-02-09 07:49:48 -0500

    ssh config

---
+++ modules/core/sshd.nix
@@ -0,0 +1,18 @@
+{ ... }:
+
+{
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
+
+    # pub keys
+    users.users.andrew.openssh.authorizedKeys.keys = [
+      # windows desktop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHpWekWY3Yov8gqTlu7U5h0hKXpA5nyUcdGs4CNsFdkx andrew@DESKTOP-BCA9DQQ"
+    ];
+  };
+}
