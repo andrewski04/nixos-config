8a1d14f3c59c8b6edae148db7f5e5d56b0d36aa6
Andrew <andrew@housermail.com>
2026-02-09 11:10:05 -0500

    updated pub keys

--- modules/core/sshd.nix
+++ modules/core/sshd.nix
@@ -13,6 +13,8 @@
     users.users.andrew.openssh.authorizedKeys.keys = [
       # windows desktop
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHpWekWY3Yov8gqTlu7U5h0hKXpA5nyUcdGs4CNsFdkx andrew@DESKTOP-BCA9DQQ"
+      # nixos laptop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWAbnpdCyp/WuUgcU3MEOXMiOIPXUxM9qgmQjj923cV andrew@nixos-laptop"
     ];
   };
 }
