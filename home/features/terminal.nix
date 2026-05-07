87c30af3049a0677a63ee01aa3fc155b869e7508
Andrew <andrew@housermail.com>
2026-05-06 21:15:16 -0400

    styling changes

--- home/features/terminal.nix
+++ home/features/terminal.nix
@@ -13,6 +13,34 @@
     settings = {
       cursor-style = "block";
       shell-integration-features = true;
+      theme = "catppuccin-mocha";
+    };
+    themes = {
+      catppuccin-mocha = {
+        background = "1e1e2e";
+        cursor-color = "f5e0dc";
+        foreground = "cdd6f4";
+        palette = [
+          "0=#45475a"
+          "1=#f38ba8"
+          "2=#a6e3a1"
+          "3=#f9e2af"
+          "4=#89b4fa"
+          "5=#f5c2e7"
+          "6=#94e2d5"
+          "7=#bac2de"
+          "8=#585b70"
+          "9=#f38ba8"
+          "10=#a6e3a1"
+          "11=#f9e2af"
+          "12=#89b4fa"
+          "13=#f5c2e7"
+          "14=#94e2d5"
+          "15=#a6adc8"
+        ];
+        selection-background = "353749";
+        selection-foreground = "cdd6f4";
+      };
     };
   };
 
@@ -63,6 +91,7 @@
     settings.vim = {
       viAlias = true;
       vimAlias = true;
+
       debugMode = {
         enable = false;
         level = 16;
@@ -72,8 +101,8 @@
       opts.expandtab = true;
 
       spellcheck = {
-        enable = true;
-        #programmingWordlist.enable = true;
+        enable = false;
+        programmingWordlist.enable = false;
       };
 
       lsp = {
@@ -84,7 +113,7 @@
         lightbulb.enable = true; # Shows a VSCode-style lightbulb for available code actions
         lspsaga.enable = false;
         trouble.enable = true; # Provides a bottom panel for project-wide errors/diagnostics
-        lspSignature.enable = true; # Shows function parameters automatically while typing
+        lspSignature.enable = true; # Shows function parameters automatically typing
         otter-nvim.enable = false;
         nvim-docs-view.enable = false;
         presets.harper.enable = false;
